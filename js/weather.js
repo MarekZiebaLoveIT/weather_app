940995838617290e5921a9fc9865e5c19e9eb4a4
Marek Zieba <marekzieba2@wp.pl>
2021-10-26 13:46:22 +0200

    fully working app

--- js/weather.js
+++ js/weather.js
@@ -36,27 +36,36 @@ getWeather().then(data => {
         container[0].classList.remove('before');
         container[1].classList.remove('after');
     });
-    
+
+
+    const whichCity = document.querySelector('#weatherCity');
+    whichCity.addEventListener('change', () => {
+        let i = whichCity.selectedIndex;
+        let cityPlace = document.querySelector('.cityPlace');
+        cityPlace.innerHTML = `${data[i-1].stacja}`;
+        let datePlace = document.querySelector('.datePlace');
+        datePlace.innerHTML = `${data[i-1].data_pomiaru}`;
+        let timePlace = document.querySelector('.timePlace');
+        timePlace.innerHTML = `${data[i-1].godzina_pomiaru}.00`;
+        let tempPlace = document.querySelector('.tempPlace');
+        tempPlace.innerHTML = `${data[i-1].temperatura} <span class="temp">&deg;C</span>`;
+        let windPlace = document.querySelector('.windPlace');
+        windPlace.innerHTML = `<img src="/assets/wind.svg" alt="windSign" height="25px"> ${data[i-1].predkosc_wiatru} m/s`;
+        let pressPlace = document.querySelector('.pressPlace');
+        pressPlace.innerHTML = `<img src="/assets/pressure.svg" alt="pressureSign" height="25px"> ${data[i-1].cisnienie} hPa`;
+        let icoPlace = document.querySelector('.icoPlace');
+        if (data[i-1].temperatura.value < 0 && data[i-1].suma_opadu.value > 1) {
+            icoPlace.innerHTML = `<img src="/assets/snowfall.png" alt="snow" width="150px">`;
+        } else if (data[i-1].temperatura.value > 0 && data[i-1].suma_opadu.value > 3) {
+            icoPlace.innerHTML = `<img src="/assets/rainy.png" alt="rainy" width="150px">`;
+        } else if (data[i-1].temperatura.value > 0 && data[i-1].suma_opadu.value == 0) {
+            icoPlace.innerHTML = `<img src="/assets/cloudy.png" alt="rainy" width="150px">`;
+        } else if (data[i-1].temperatura.value > 0 && data[i-1].suma_opadu.value > 10  && data[i-1].cisnienie.value < 998) {
+            icoPlace.innerHTML = `<img src="/assets/rain.png" alt="rainy" width="150px">`;
+        } else {
+            icoPlace.innerHTML = `<img src="/assets/sun.png" alt="sunny" width="150px">`;
+        }
+    });
     
 });
 
-
-
-
-
-/*
-const whichCity = document.querySelector('#weatherCity');
-whichCity.addEventListener('change', () => {
-    let i = whichCity.selectedIndex;
-    let tr = document.createElement('tr');
-    tr.innerHTML = `
-    <td>${data[i-1].stacja}</td>
-    <td>${data[i-1].data_pomiaru}</td>
-    <td>${data[i-1].godzina_pomiaru}</td>
-    <td>${data[i-1].temperatura}</td>
-    <td>${data[i-1].suma_opadu}</td>
-    <td>${data[i-1].cisnienie}</td>
-    `;
-    document.querySelector('table tbody').appendChild(tr);
-});
-*/
